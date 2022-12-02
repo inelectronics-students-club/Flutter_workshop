a2eb746aa3d8c3119d0ca4cb80c99296b242db01
Inelectronics Students Club <94116296+inelectronics-students-club@users.noreply.github.com>
2022-12-02 19:33:30 +0100

    Delete lib directory

--- lib/constants.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kAppBarColor = Color.fromARGB(255, 64, 65, 65);
-const kBodyBcolor = Color.fromARGB(255, 217, 223, 226);
-const kDrawercolor = Color(0xFF979DAC);
